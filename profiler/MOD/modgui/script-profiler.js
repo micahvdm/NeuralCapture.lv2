8cd32ad084c9014e96bfa814cfba957b7d7e8b50
brummer10 <brummer-@web.de>
2023-08-17 10:22:24 +0200

    Use custom popup instead alert window to show errors in MOD UI

--- profiler/MOD/modgui/script-profiler.js
+++ profiler/MOD/modgui/script-profiler.js
@@ -1,5 +1,10 @@
 function (event) {
 
+    const clip = event.icon.find ('[mod-role=CLIP]');
+    const meter = event.icon.find ('[mod-role=METER]');
+    const state = event.icon.find ('[mod-role=STATE]');
+    const popup = event.icon.find ('[mod-role=POPUP]');
+
     var position = "0";
 
     function log_meter (db) {
@@ -31,34 +36,38 @@ function (event) {
             case 'CLIP':
                 position = (Math.min(Math.floor(value), 1)).toFixed(1);
                 if ( position >= 1) {
-                    event.icon.find ('[mod-role=CLIP]').css({background: `red`});
-                    event.icon.find ('[mod-role=CLIP]').css({boxShadow: `-5px -5px 15px red, 2px 2px 3px rgba(255,255,255,0.1)`});
+                    clip.css({background: `red`});
+                    clip.css({boxShadow: `-5px -5px 15px red, 2px 2px 3px rgba(255,255,255,0.1)`});
                 } else {
-                    event.icon.find ('[mod-role=CLIP]').css({background: `#2b2b2b`});
-                    event.icon.find ('[mod-role=CLIP]').css({boxShadow: `2px 2px 3px rgba(255,255,255,0.1)`});
+                    clip.css({background: `#2b2b2b`});
+                    clip.css({boxShadow: `2px 2px 3px rgba(255,255,255,0.1)`});
                 }
                 break;
             case 'METER':
                 position = log_meter (value);
-                event.icon.find ('[mod-role=METER]').animate({height: position + `%`});
+                meter.animate({height: position + `%`});
                 if (value >= 0) {
-                    event.icon.find ('[mod-role=METER]').css({boxShadow: `5px 5px 15px red`});                    
+                    meter.css({boxShadow: `5px 5px 15px red`});                    
                 } else if (value > -6) {
-                    event.icon.find ('[mod-role=METER]').css({boxShadow: `5px 5px 15px rgba(242, 121, 0, 0.8)`});
+                    meter.css({boxShadow: `5px 5px 15px rgba(242, 121, 0, 0.8)`});
                 } else {
-                    event.icon.find ('[mod-role=METER]').css({boxShadow: `5px 5px 15px rgba(35, 145, 0, 0.8)`});
+                    meter.css({boxShadow: `5px 5px 15px rgba(35, 145, 0, 0.8)`});
                 }
                 break;
             case 'STATE':
                 position = (Math.floor(value * 100)).toFixed(1);
-                event.icon.find ('[mod-role=STATE]').css({width: `${position}%`});
-                event.icon.find ('[mod-role=STATE]').text(`${position}%`);
+                state.css({width: `${position}%`});
+                state.text(`${position}%`);
                 break;
             case 'ERRORS':
                 if (value >= 2.0) {
-                    alert("Profiler: Seems we receive only garbage, we stop the profiling process here");
+                    popup.text(`Profiler Error: Seems we receive only garbage, we stop the profiling process now`);
+                    popup.css({display: 'block'});
+                    setTimeout(function() { popup.css({display: 'none'}); }, 5000); 
                 } else if (value >= 1.0) {
-                    alert("Profiler: No signal comes in, we stop the profiling process here");
+                    popup.text(`Profiler Error: No signal comes in, we stop the profiling process now`);
+                    popup.css({display: 'block'});
+                    setTimeout(function() { popup.css({display: 'none'}); }, 5000); 
                 }
                 break;
             default:
